ac834cc0f120f2690f79a66dcf3e168db0b05d5d
piyush1407 <102173748+Daksh1407@users.noreply.github.com>
2023-04-30 10:19:51 +0530

    Added solution - LeetHub

--- Longest Increasing Subsequence - GFG/longest-increasing-subsequence.cpp
+++ Longest Increasing Subsequence - GFG/longest-increasing-subsequence.cpp
@@ -12,7 +12,7 @@ class Solution
     int longestSubsequence(int n, int nums[]) {
         //int n = nums.size();
         int ans = INT_MIN;
-        vector<int>dp(n);
+        vector<int>dp(n, 1);
 
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < i; j++) {
@@ -21,7 +21,7 @@ class Solution
             }
             ans = max(ans, dp[i]);
         }
-        return ans + 1;
+        return ans;
     }
 };
 
