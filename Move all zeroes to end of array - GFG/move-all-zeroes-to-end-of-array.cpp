5ad36b2f293d891bffcd49da27ae0511dd47b0d1
piyush1407 <102173748+Daksh1407@users.noreply.github.com>
2023-10-31 11:54:36 +0530

    Added solution - LeetHub

---
+++ Move all zeroes to end of array - GFG/move-all-zeroes-to-end-of-array.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	void pushZerosToEnd(int nums[], int n) {
+	    int k = -1;
+	    
+	    for(int i = 0; i < n; i++) {
+	        if(nums[i] != 0) {
+	            k++;
+	            swap(nums[i], nums[k]);
+	        }
+	        else
+	            continue;
+	    }
+	}
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        ob.pushZerosToEnd(arr, n);
+        for (i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
