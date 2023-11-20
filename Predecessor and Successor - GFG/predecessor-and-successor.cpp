82accf732684c6a5ed95a4b207bef14fb230a02e
piyush1407 <102173748+Daksh1407@users.noreply.github.com>
2023-11-20 11:59:33 +0530

    Added solution - LeetHub

--- Predecessor and Successor - GFG/predecessor-and-successor.cpp
+++ Predecessor and Successor - GFG/predecessor-and-successor.cpp
@@ -39,40 +39,40 @@ struct Node
 class Solution
 {
     public:
-    void prede(Node* root, Node*& pre, int key)
-    {
-        while(root!=0)
-        {
-            if(key<=root->key)
-            {
-                root=root->left;
+    
+    void pref(Node* root, Node* &pre, int key) {
+        if(!root)
+            return;
+        
+        while(root != 0) {
+            if(key <= root->key) {
+                root = root->left;
             }
-            else
-            {
-                pre=root;
-                root=root->right;
+            else {
+                pre = root;
+                root = root->right;
             }
         }
     }
-    void succe(Node* root, Node*& suc, int key)
-    {
-        while(root!=0)
-        {
-            if(key>= root->key)
-            {
-                root=root->right;
+    void sucf(Node* root, Node* &suc, int key) {
+        if(!root)
+            return;
+        
+        while(root != 0) {
+            if(key >= root->key) {
+                root = root->right;
             }
-            else
-            {
-                suc=root;
-                root=root->left;
+            else {
+                suc = root;
+                root = root->left;
             }
         }
     }
     void findPreSuc(Node* root, Node*& pre, Node*& suc, int key)
     {
-        prede(root,pre,key);
-        succe(root,suc,key);
+        pref(root, pre, key);
+        sucf(root, suc, key);
+        // Your code goes here
     }
 };
 
